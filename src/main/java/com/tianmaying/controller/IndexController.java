e1c1e4c6051a5ac0a92e86aa509459c1c5ea8d79
qscde45 <qscde42@gmail.com>
2018-02-10 18:44:32 -0500

    to modify list.html

--- src/main/java/com/tianmaying/controller/IndexController.java
+++ src/main/java/com/tianmaying/controller/IndexController.java
@@ -1,27 +1,39 @@
 package com.tianmaying.controller;
 
+import com.tianmaying.model.User;
+import com.tianmaying.model.Blog;
 import com.tianmaying.service.BlogService;
+import com.tianmaying.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
 public class IndexController {
 
     private final BlogService blogService;
+    private final UserService userService;
 
     @Autowired
-    public IndexController(BlogService blogService) {
+    public IndexController(BlogService blogService, UserService userService) {
         this.blogService = blogService;
+        this.userService = userService;
     }
+
+
 
     @GetMapping("/{username}")
     //使用@RequestParam获取参数
     public String get(@PathVariable("username") String username, Model model) {
         // Your Code goes here
         // 渲染模板list.html
+        User user = userService.findByName(username);
+        List<Blog> bloglists = blogService.findBlogs(user);
+        model.addAttribute("bloglists", bloglists);
         return "list";
     }
 
